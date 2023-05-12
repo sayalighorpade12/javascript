bec6c151a008cc917640af77574a05ef2740df3c
sayalighoropade <ghorpadesayali10@gmail.com>
2023-05-12 08:02:05 +0530

    hello.js

---
+++ hello.js
@@ -0,0 +1,20 @@
+console.log("sayali ghorpade..");
+
+var firstName = "sayali";
+console.log(firstName);
+
+var lastName = "ghorpade";
+console.log(lastName);
+
+var age; // Variable Declaration
+age = "23"; // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); //Undefined
+holder = "My laptop"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; //Variable Uodate
+ console.log(holder);
+// When we declare thr variable and does not initialize the value then its undefined
