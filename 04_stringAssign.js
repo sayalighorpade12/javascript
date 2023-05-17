7cb9be8d800efd91b2861a4f88f316f33fee0a1e
sayalighoropade <ghorpadesayali10@gmail.com>
2023-05-17 12:42:56 +0530

    String Assignment01

--- 04_stringAssign.js
+++ 04_stringAssign.js
@@ -18,37 +18,6 @@ function stringBasic() {
 }
 stringBasic();
 
-console.log("                                                                                 ");
-console.log("-------------------------------Assignment2---------------------------------------");
-console.log("                                                                                 ");
-
-
-
-
-function stringHandsOn(){
-    var givenString ="   Hey you are doing good, keep it up    ";
-    console.log("1. Given a string : ", givenString);
-
-      var result = givenString.length;
-      console.log("2. Total Length of the given string is : ", result);
-
-       var result = givenString.trim();
-      console.log("3. After remove leading and trailling extra spaces length is : ", result.length );
-   
-      var givenStringLength = givenString.length;
-      var result1 = result.length;
-       console.log("4. Total number of removed extra spaces count are : ", givenStringLength-result1);
-
-       var givenString1 ="Hey you are doing good, keep it up";
-
-       var charAtFirstPlace = givenString1.charAt(0);
-       var charAtLastPlace = givenString1.charAt(givenString1.length-1);
-       console.log("5. First and last character of given string after remove extra spaces : ",charAtFirstPlace, charAtLastPlace);
-
-      
-}
-
-stringHandsOn();
 
 
 
