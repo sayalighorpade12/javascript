9286c78d9d4c463c8adc29ebe6709cbef1fb7707
sayalighoropade <ghorpadesayali10@gmail.com>
2023-05-17 12:38:37 +0530

    Function Expression

---
+++ 03_functonExpression.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+var multiplication = function (one, two, three) {
+    var result = one*two*three;
+    return `Multiplication is : ${result}`;
+}
+var result = multiplication(2,3,5);
+console.log(result);
+
+
+
