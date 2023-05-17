0913469eb06b50b7eb7899f28ab816bf0dc8613b
sayalighoropade <ghorpadesayali10@gmail.com>
2023-05-17 12:34:54 +0530

    Variable Basic Assignment

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,24 @@
+
+
+var pin = 411052;
+
+
+var typeOfPin = typeof pin;
+console.log("type: ", pin);
+
+var pin = 411052;
+var typeOfPin = typeof pin;
+console.log("Pin:", pin , "Type:", typeOfPin);
+
+var numberOfPeople = 200;
+var typeOfnumberOfPeople = typeof numberOfPeople; 
+console.log("type:",  typeOfnumberOfPeople);
+
+
+
+
+
+
+
+
+
