0bd6e1181c451385828dc528729565dabc42cb57
sayalighoropade <ghorpadesayali10@gmail.com>
2023-06-09 08:04:04 +0530

    urnary operator

---
+++ 05_unaryOpertor.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+var num1 = 5;
+var num1 = num1 + 1;
+console.log(`${num1}`);
+
+console.log(`Increment Operator - Post fix`);
+var num2 = 10;
+var num2 = num2++; // num2 + 1 =11;
+console.log(`Post increment Operator: ${num2}`);
+
+console.log(`Increment Operator - Pre fix`);
+var num3 = 15;
+var num3 = ++num3; // num3 + 1 =16;
+console.log(`Pre increment Operator: ${num3}`);
+
+console.log(`Decrement Operator - Post fix`);
+var num5 = 25;
+var num5 = num5--; // num5 - 1 =16;
+console.log(`Post decrement Operator: ${num5}`);
+
+console.log(`Decrement Operator - Pre fix`);
+var num4 = 20;
+var num4 = --num4; // num4 - 1 =19;
+console.log(`Pre decrement Operator: ${num4}`);
+
+
